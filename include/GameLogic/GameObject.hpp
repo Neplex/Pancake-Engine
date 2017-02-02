396547c0cefbede460596d912b3da4eafbb6418d
Darenn Keller <keller.darenn@gmail.com>
2017-02-02 10:52:02 +0100

    Documented Gameobject.

--- include/GameLogic/GameObject.hpp
+++ include/GameLogic/GameObject.hpp
@@ -1,6 +1,27 @@
-//
-// Created by Darenn on 12/01/2017.
-//
+/*
+     Copyright (C) 2016-2017 Keller Darenn - keller.darenn@gmail.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+/**
+ * \file        GameObject.hpp
+ * \author      Darenn Keller - keller.darenn@gmail.com
+ * \author      Nicolas Hiot
+ */
 
 #ifndef PANCAKE_GAMEOBJECT_HPP
 #define PANCAKE_GAMEOBJECT_HPP
@@ -14,23 +35,27 @@ class Component;
 
 namespace PancakeEngine {
 
+    /**
+     * @class GameObject
+     * @brief Represent an object in the world (Scene).
+     * @details A gameobject can only be create in a scene.
+     * As a consequence it has a Transform.
+     * A gameobject has components that define the behavior and the nature of the gameobject.
+     * @sa Scene Component Transform
+     */
     class GameObject {
     public:
-        std::string name;
-        Transform transform;
-
-        GameObject();
-        ~GameObject() {
-            for (Component* c : components) {
-                delete c;
-            }
-        }
+        std::string name; ///< The name of the gameobject
+        Transform transform; ///< The transform of the gameobject
 
         /**
-         * Return the first component of the type T in the game object.
-         * Use of dynamic_cast
+         * @brief Return the first component of the type T in the game object.
+         * @details Use of dynamic_cast
          * @tparam T The type of the component.
          * @return The first component of the type T, NULL if no component of the type T.
+         * @attention Please do not mess up with the pointer.
+         * @todo pointer is too permissive ?
+         * @sa Component
          */
         template<typename T>
         T* getComponent()
@@ -44,10 +69,12 @@ namespace PancakeEngine {
         }
 
         /**
-         * Return the list of components of the type T in the game object.
-         * Use of dynamic_cast
+         * @brief Return the list of components of the type T in the game object.
+         * @details Use of dynamic_cast
          * @tparam T The type of the component.
          * @return The vector of all component of the type T.
+         * @attention Please do not mess up with the pointer.
+         * @todo pointer is too permissive ?
          */
         template<typename T>
         const std::vector<T*> getComponents()
@@ -63,9 +90,10 @@ namespace PancakeEngine {
 
 
         /**
-         *
+         * @brief Add a component to the gameobject
          * @tparam T should inherit from Component.
-         * @return
+         * @return The component created.
+         * @sa Component
          */
         template <class T>
         T& addComponent() {
@@ -76,31 +104,38 @@ namespace PancakeEngine {
         }
 
         /**
-         * Called at GameObject creation.
-         * Call the same method on all components in order.
+         * @brief Called at GameObject creation.
+         * @details Call the same method on all components in order.
          */
         void awake();
 
         /**
-         * Called just before the first Update call.
-         * Call the same method on all components in order.
+         * @brief Called just before the first Update call.
+         * @details Call the same method on all components in order.
          */
         void start();
 
         /**
-         * Called at each frame before physics.
-         * Call the same method on all components in order.
+         * @brief Called at each update engine call.
+         * @details Call the same method on all components in order.
          */
         void update();
 
         /**
-         * Called at each frame after physics.
-         * Call the same method on all components in order.
+         * @brief Called at each frame after the update engine call.
+         * @details Call the same method on all components in order.
          */
         void lateUpdate();
 
-    private:
-        std::vector<Component*> components;
+    protected:
+        friend class Scene; ///< The scene is the only one to create and destroy gameobjects
+        GameObject();
+        ~GameObject() {
+            for (Component* c : components) {
+                delete c;
+            }
+        }
+        std::vector<Component*> components; ///< The components of the gameobject in the added order.
     };
 }
 
