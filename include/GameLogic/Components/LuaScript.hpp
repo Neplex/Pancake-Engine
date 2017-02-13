e397eea8b0bf2e1ab7760a0c0e3ac72cd8b5e068
Nicolas Hiot <nicolas.hiot@etu.univ-orleans.fr>
2017-02-13 12:49:29 +0100

    Add documentation

--- include/GameLogic/Components/LuaScript.hpp
+++ include/GameLogic/Components/LuaScript.hpp
@@ -1,6 +1,26 @@
-//
-// Created by nicolas on 02/02/17.
-//
+/*
+     Copyright (C) 2016-2017 Nicolas Hiot - nicolashiot@hotmail.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+*/
+
+/**
+ * \file        LuaScript.hpp
+ * \author      Nicolas Hiot - nicolashiot@hotmail.com
+ */
 
 #ifndef PANCAKE_LUASCRIPT_HPP
 #define PANCAKE_LUASCRIPT_HPP
@@ -12,14 +32,17 @@
 #include <lauxlib.h>
 
 namespace PancakeEngine {
+
     /**
-     * @brief Component that can run a lua script
-     * All component methods will call the lua function with the same name in the current script
+     * @class LuaScript
+     * @brief Component that can run a lua script.
+     * @details All component methods will call the lua function with the same name in the current script.
+     * @sa Component
      */
     class LuaScript : public Component {
     public:
         /**
-         * @brief Set an run a new script
+         * @brief Set and run a new script
          * @param uri the uri of the script
          */
         void setScript(std::string uri);
@@ -32,7 +55,7 @@ namespace PancakeEngine {
     private:
         friend class GameObject;
 
-        lua_State *L;
+        lua_State *L; ///< Lua state machine.
 
         LuaScript();
         ~LuaScript();
