8182019faa90b75c1feb4824bc715a58ff2dd13d
Darenn Keller <keller.darenn@gmail.com>
2017-03-17 21:19:20 +0100

    Added comments for the report.

--- include/User/Behaviors/OnTriggerKill.hpp
+++ include/User/Behaviors/OnTriggerKill.hpp
@@ -10,14 +10,15 @@
 
 class OnTriggerKill : public PancakeEngine::Behavior {
 public:
-
+    // OnTriggerEnter is called when another collider enter the trigger of this object
     void OnTriggerEnter(const PancakeEngine::Collider &triggered, const PancakeEngine::Collider &other) override {
+        // Health is another Behavior that gives the GameObject health and the possibility to die
         Health* h = other.gameObject->getComponent<Health>();
         if (h) {
+            // Here we kill the object
             h->kill();
         }
     }
-
 };
 
 #endif //PANCAKE_ONTRIGGERKILL_HPP
