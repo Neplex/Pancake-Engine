e8d6c96cdb2a0d5342f64fa82347f3cec2ee2bed
vpia <virgil.pia@gmail.com>
2017-03-17 22:55:29 +0100

    Move function implementation to .cpp

--- include/Physics/PhysicsListener.hpp
+++ include/Physics/PhysicsListener.hpp
@@ -56,36 +56,7 @@ namespace PancakeEngine {
         }
 
         // Helper functions
-        void onContact(b2Contact* contact, void (GameObject::*triggerFunction)(const Collider&, const Collider&), void (GameObject::*collisionFunction)(const Collision&), bool exiting) {
-            if (contact->IsTouching() || exiting) {
-                // If only one of them is a sensor call OnTrigger on the sensor GameObject
-                if (contact->GetFixtureA()->IsSensor() ^ contact->GetFixtureB()->IsSensor()) {
-                    const Collider* sensor;
-                    const Collider* other;
-                    if (contact->GetFixtureA()->IsSensor()) {
-                        sensor = static_cast<Collider *>(contact->GetFixtureA()->GetUserData());
-                        other = static_cast<Collider *>(contact->GetFixtureB()->GetUserData());
-                    } else if (contact->GetFixtureB()->IsSensor()) {
-                        sensor = static_cast<Collider *>(contact->GetFixtureB()->GetUserData());
-                        other = static_cast<Collider *>(contact->GetFixtureA()->GetUserData());
-                    }
-                    (sensor->gameObject->*triggerFunction)(*sensor, *other);
-                    // If none of them is a sensor, call OnCollision on both ot them
-                } else if (!contact->GetFixtureA()->IsSensor() && !contact->GetFixtureB()->IsSensor()) {
-                    const Collision collA(*(static_cast<Collider *>(contact->GetFixtureA()->GetUserData())),
-                                          *(static_cast<Collider *>(contact->GetFixtureB()->GetUserData())),
-                                          contact->GetRestitution(), contact->GetFriction(),
-                                          contact->GetTangentSpeed());
-                    (static_cast<Collider *>(contact->GetFixtureA()->GetUserData())->gameObject->*collisionFunction)(collA);
-
-                    const Collision collB(*(static_cast<Collider *>(contact->GetFixtureB()->GetUserData())),
-                                          *(static_cast<Collider *>(contact->GetFixtureA()->GetUserData())),
-                                          contact->GetRestitution(), contact->GetFriction(),
-                                          contact->GetTangentSpeed());
-                    (static_cast<Collider *>(contact->GetFixtureB()->GetUserData())->gameObject->*collisionFunction)(collB);
-                } // Else if both are sensor do nothing
-            } // Else if fixtures are not touching do nothing
-        }
+        void onContact(b2Contact* contact, void (GameObject::*triggerFunction)(const Collider&, const Collider&), void (GameObject::*collisionFunction)(const Collision&), bool exiting);
     };
 
 }
--- include/User.hpp
+++ include/User.hpp
@@ -24,84 +24,9 @@
  * @defgroup User User
  * Contains all classes concerning the User Prefab object.
  */
-namespace PancakeEngine{
     /**
      * @brief create all spritesheet, prefab and input for player
      */
-    void CreatePrefabAndSpriteAndInput() {
-        AssetsManager::createSpriteSheet("player1", "../resources/players/player1.png", 70, 100);
-        AssetsManager::createSpriteSheet("player2", "../resources/players/player2.png", 70, 100);
-        AssetsManager::createSpriteSheet("player3", "../resources/players/player3.png", 70, 100);
-        AssetsManager::createSpriteSheet("player4", "../resources/players/player4.png", 70, 100);
-        AssetsManager::createSpriteSheet("player5", "../resources/players/player5.png", 70, 100);
-        AssetsManager::createSpriteSheet("tiles", "../resources/tiles/tiles.png", 70, 70);
-        AssetsManager::createSpriteSheet("miscs", "../resources/tiles/miscs.png", 70, 70);
-        AssetsManager::createSpriteSheet("items", "../resources/items.png", 70, 70);
-        AssetsManager::createSpriteSheet("slimeGreen", "../resources/enemies/slimeGreen.png", 57, 34);
-        AssetsManager::createSpriteSheet("slimePink", "../resources/enemies/slimePink.png", 57, 34);
-        AssetsManager::createSpriteSheet("slimeBlue", "../resources/enemies/slimeBlue.png", 57, 34);
-        AssetsManager::createSpriteSheet("heart", "../resources/heart.png", 53, 45);
-        AssetsManager::createSpriteSheet("hud", "../resources/hud.png", 47, 47);
+    void CreatePrefabAndSpriteAndInput();
 
-        SceneFactory::addPrefab<Coin>("Coin");
-        SceneFactory::addPrefab<Player1>("Player");
-        SceneFactory::addPrefab<Player1>("Player1");
-        SceneFactory::addPrefab<Player2>("Player2");
-        SceneFactory::addPrefab<Player3>("Player3");
-        SceneFactory::addPrefab<Player4>("Player4");
-        SceneFactory::addPrefab<Player5>("Player5");
-        SceneFactory::addPrefab<Box>("Box");
-        SceneFactory::addPrefab<Rock>("Rock");
-        SceneFactory::addPrefab<SmallRock>("SmallRock");
-        SceneFactory::addPrefab<Ladder>("Ladder");
-        SceneFactory::addPrefab<Water>("Water");
-        SceneFactory::addPrefab<Laser>("Laser");
-        SceneFactory::addPrefab<DeathZone>("DeathZone");
-        SceneFactory::addPrefab<Transporter1>("Transporter1");
-        SceneFactory::addPrefab<Transporter2>("Transporter2");
-        SceneFactory::addPrefab<Transporter3>("Transporter3");
-        SceneFactory::addPrefab<Trampoline>("Trampoline");
-        SceneFactory::addPrefab<CheckPoint1>("CheckPoint1");
-        SceneFactory::addPrefab<CheckPoint2>("CheckPoint2");
-        SceneFactory::addPrefab<CheckPoint3>("CheckPoint3");
-        SceneFactory::addPrefab<CheckPoint4>("CheckPoint4");
-        SceneFactory::addPrefab<LaserButton>("LaserButton");
-        SceneFactory::addPrefab<Enemy1>("slimeGreen");
-        SceneFactory::addPrefab<Enemy2>("slimePink");
-        SceneFactory::addPrefab<Enemy3>("slimeBlue");
-
-        InputManager::createButton("player1_jump", std::vector<sf::Keyboard::Key> ({sf::Keyboard::Key::Z}));
-        InputManager::createButton("player1_fire", std::vector<sf::Keyboard::Key> ({sf::Keyboard::Key::Space}));
-        InputManager::createButton("player1_right", std::vector<sf::Keyboard::Key> ({sf::Keyboard::Key::D}));
-        InputManager::createButton("player1_left", std::vector<sf::Keyboard::Key> ({sf::Keyboard::Key::Q}));
-        InputManager::createButton("player1_duck", std::vector<sf::Keyboard::Key> ({sf::Keyboard::Key::S}));
-
-        InputManager::createButton("player2_jump", std::vector<sf::Keyboard::Key> ({sf::Keyboard::Key::Up}));
-        InputManager::createButton("player2_fire", std::vector<sf::Keyboard::Key> ({sf::Keyboard::Key::RControl}));
-        InputManager::createButton("player2_right", std::vector<sf::Keyboard::Key> ({sf::Keyboard::Key::Right}));
-        InputManager::createButton("player2_left", std::vector<sf::Keyboard::Key> ({sf::Keyboard::Key::Left}));
-        InputManager::createButton("player2_duck", std::vector<sf::Keyboard::Key> ({sf::Keyboard::Key::Down}));
-
-        InputManager::createButton("player3_jump", std::vector<sf::Keyboard::Key> ({}));
-        InputManager::createButton("player3_fire", std::vector<sf::Keyboard::Key> ({}));
-        InputManager::createButton("player3_right", std::vector<sf::Keyboard::Key> ({}));
-        InputManager::createButton("player3_left", std::vector<sf::Keyboard::Key> ({}));
-        InputManager::createButton("player3_duck", std::vector<sf::Keyboard::Key> ({}));
-
-        InputManager::createButton("player4_jump", std::vector<sf::Keyboard::Key> ({}));
-        InputManager::createButton("player4_fire", std::vector<sf::Keyboard::Key> ({}));
-        InputManager::createButton("player4_right", std::vector<sf::Keyboard::Key> ({}));
-        InputManager::createButton("player4_left", std::vector<sf::Keyboard::Key> ({}));
-        InputManager::createButton("player4_duck", std::vector<sf::Keyboard::Key> ({}));
-
-        InputManager::createButton("player5_jump", std::vector<sf::Keyboard::Key> ({}));
-        InputManager::createButton("player5_fire", std::vector<sf::Keyboard::Key> ({}));
-        InputManager::createButton("player5_right", std::vector<sf::Keyboard::Key> ({}));
-        InputManager::createButton("player5_left", std::vector<sf::Keyboard::Key> ({}));
-        InputManager::createButton("player5_duck", std::vector<sf::Keyboard::Key> ({}));
-
-        InputManager::createButton("ShowDebugInfo", std::vector<sf::Keyboard::Key> ({sf::Keyboard::Key::F1}), Debug::switchEnableDebugGUI);
-    };
-
-};
 #endif //PANCAKE_USER_HPP
